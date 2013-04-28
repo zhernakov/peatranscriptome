9dd0e0ee01c3caee40ab72f528f9aa945ada2122
alexander <azhernakov@gmail.com>
2013-04-28 22:32:11 +0400

    some project setings

--- NGSAnalyser/src/ngsanalyser/NGSAnalyser.java
+++ NGSAnalyser/src/ngsanalyser/NGSAnalyser.java
@@ -1,8 +1,12 @@
 package ngsanalyser;
 
+import java.io.File;
+import java.io.IOException;
+
 public class NGSAnalyser {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("hello world");
+
     }
 }
