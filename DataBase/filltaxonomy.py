d524345825b92636ecc31523871bd7186139e1af
alexander <azhernakov@gmail.com>
2013-05-07 04:32:10 +0400

    variant

--- DataBase/filltaxonomy.py
+++ DataBase/filltaxonomy.py
@@ -1,35 +1,37 @@
 import mysql.connector
 from struct import *
 
+path = "../_data/taxonomy_rs"
 
-host = "193.218.141.85"
+host = "192.168.0.199"
 port = "1981"
 user = "alexander"
 password = "1981ujlfhj;ltybz"
 database = "peatranscriptome"
 
-path = "taxonomy"
 
 def getValue(ar):
-        ba = bytearray([ar[2],ar[1],ar[0],0,ar[5],ar[4],ar[3],0])
-        return unpack('II', ba)
+        return unpack('II', bytearray([ar[2],ar[1],ar[0],0,ar[5],ar[4],ar[3],0]))
 
-f = open(path, 'rb')
-
-count = 0 
-while True:
-        read = f.read(6)
-        if not read: break
-        #fr = f.read(3)
-        #sr = f.read(3)
-        #if not fr or not sr: break
-        taxid, parent = getValue(read)
-        #taxid = getValue(fr)
-        #parent = getValue(sr)
-        print(taxid, parent)
-        count += 1
-
-print (count)
+class Processor:
+        def __init__(self, cnc):
+                self.connection = cnc
+                self.count = 0
+                self.query = None
+        def add(self, values):
+                if not self.query:
+                        self.query = 'INSERT INTO transcriptom VALUES '
+                self.query += '(' + str(values[0]) + ',' + str(values[0]) + '),'
+                self.count += 1
+                if self.count > 100:
+                        self.flush()
+        def flush(self):
+                if self.query:
+                        print (self.query)
+                        self.query = None
+                        self.count = 0
+                        
+                
 
 connection = mysql.connector.connect(
         host=host,
@@ -37,6 +39,21 @@ connection = mysql.connector.connect(
         user=user,
         password=password,
         database=database)
+
+processor = Processor(connection)
+
+f = open(path, 'rb')
+
+count = 0 
+while True:
+        read = f.read(6)
+        if not read:
+                break
+        processor.add(getValue(read))
+        count += 1
+
+print (count)
+
  
 cursor = connection.cursor()
  
@@ -45,5 +62,5 @@ cursor.execute("SELECT * FROM taxonomy")
 for (id) in cursor:
 	print (id)
 
-
-connection.close();
+f.close
+connection.close()
