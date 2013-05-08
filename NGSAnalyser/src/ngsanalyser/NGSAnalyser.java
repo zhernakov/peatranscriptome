4d2b7b78d51a76280b1bf8bf1ba45f0dc6bdebcd
alexander <azhernakov@gmail.com>
2013-05-08 19:20:55 +0400

    Experiment class

--- NGSAnalyser/src/ngsanalyser/NGSAnalyser.java
+++ NGSAnalyser/src/ngsanalyser/NGSAnalyser.java
@@ -2,6 +2,8 @@ package ngsanalyser;
 
 import ngsanalyser.ngsdata.NGSRecordsCollection;
 import com.beust.jcommander.JCommander;
+import java.io.IOException;
+import ngsanalyser.exception.ParsingException;
 import ngsanalyser.processes.blaster.BLASTer;
 import ngsanalyser.processes.hitsanalyzer.HitsAnalyzer;
 import ngsanalyser.ngsdata.NGSFile;
@@ -11,31 +13,30 @@ import ngsanalyser.taxonomy.TaxonomyException;
 
 public class NGSAnalyser {
 
-    public static void main(String[] args) throws NGSFileException, InterruptedException, TaxonomyException {
+    public static void main(String[] args) throws NGSFileException, InterruptedException, TaxonomyException, ParsingException, IOException {
         final Settings settings = new Settings();
         new JCommander(settings, args);
         
-        System.out.println(settings.infofile);
+        System.out.println(settings.experiment);
         System.out.println(settings.ngsfile);
         
-        Taxonomy.loadSource();
+        Experiment info = Experiment.createInstance(settings.experiment);
         
-        final NGSFile fastqfile = NGSFile.NGSFileFactory(settings.ngsfile);
+        int c = 0;
         
-        final NGSRecordsCollection failedstorage = new NGSRecordsCollection();
-        final NGSRecordsCollection blaststorage = new NGSRecordsCollection();
-        final NGSRecordsCollection analysisstorage = new NGSRecordsCollection();
+//        Taxonomy.loadSource();
+//        
+//        final NGSFile fastqfile = NGSFile.NGSFileFactory(settings.ngsfile);
+//        
+//        final NGSRecordsCollection failedstorage = new NGSRecordsCollection();
+//        final NGSRecordsCollection blaststorage = new NGSRecordsCollection();
+//        final NGSRecordsCollection analysisstorage = new NGSRecordsCollection();
+//        
+//        final BLASTer blaster = new BLASTer(fastqfile, blaststorage, failedstorage, 14);
+//        final HitsAnalyzer analyzer = new HitsAnalyzer(blaststorage, analysisstorage, failedstorage, 20);
+//        
+//        blaster.startBLAST();
+//        analyzer.startAnalysis();
         
-        final BLASTer blaster = new BLASTer(fastqfile, blaststorage, failedstorage, 14);
-        final HitsAnalyzer analyzer = new HitsAnalyzer(blaststorage, analysisstorage, failedstorage, 20);
-        
-        blaster.startBLAST();
-        analyzer.startAnalysis();
-        
-        System.out.println(failedstorage.getNumber());
-        
-//        final Processor processor = new Processor(fastqfile);
-//        processor.startAnalysis();
-
     }
 }
--- NGSAnalyser/src/ngsanalyser/Settings.java
+++ NGSAnalyser/src/ngsanalyser/Settings.java
@@ -3,9 +3,12 @@ package ngsanalyser;
 import com.beust.jcommander.Parameter;
 
 public class Settings {
-    @Parameter(names = {"-i", "--infofile"}, description = "analysis information", required = true)
-    public String infofile;
+    @Parameter(names = {"-e", "--experiment"}, description = "experiment information", required = true)
+    public String experiment;
     
     @Parameter(names= {"-s", "--ngsfile"}, description = "file reads", required = true)
     public String ngsfile;
+
+    @Parameter(names= {"-h", "--host"}, description = "bd host", required = false)
+    public String host;
 }
